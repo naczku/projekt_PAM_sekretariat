5255bf2f4dd58eb2b2876825377eebd67a2d813b
naczku <damiannaczk1@gmail.com>
2022-01-14 15:21:26 +0100

    GUI

--- app/src/main/java/com/example/projektpamskreta/MainActivity.kt
+++ app/src/main/java/com/example/projektpamskreta/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.projektpamskreta
 
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        title= "KotlinApp"
+        val button:Button =findViewById(R.id.openNauczyciel)
+        button.setOnClickListener{
+            val intent = Intent(this@MainActivity , nauczyciele::class.java )
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/projektpamskreta/nauczyciele.kt
@@ -0,0 +1,14 @@
+package com.example.projektpamskreta
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.
+
+class nauczyciele : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nauczyciele)
+
+    }
+}
